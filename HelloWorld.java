63db5755b31a65e93da697adcb5a512e91393352
radhakrishn2021 <100672009+radhakrishn2021@users.noreply.github.com>
2022-03-03 02:44:35 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
  public static void main(String args[]){
-System.out.println("Hello Jenkins have a good day  ");
+System.out.println("Hello Jenkins i am coming from git hub  ");
 }
 }
